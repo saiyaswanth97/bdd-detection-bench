96b102a36738fba658ddeb668c2f763cbdc4f466
ymummaneni <smummaneni@monarchtractor.com>
2026-02-15 22:21:09 +0530

    encapsule config

--- src/task2/main.py
+++ src/task2/main.py
@@ -26,34 +26,45 @@ class BDDTrainer(DefaultTrainer):
         )
 
 
-if __name__ == "__main__":
-    setup_logger()
-
+def get_custom_config(epochs, len_epoch):
     cfg = get_cfg()
     cfg.merge_from_file(
         model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml")
     )
-    train_dataset = make_coco_dicts("train")
-    val_dataset = make_coco_dicts("val")
-
-    cfg.DATASETS.TRAIN = ("bdd_dataset_train",)
-    cfg.DATASETS.TEST = ("bdd_dataset_val",)
+    # cfg.SOLVER.AMP.ENABLED = False    # Errors wrt to new torch version
     cfg.DATALOADER.NUM_WORKERS = 4
+    cfg.SOLVER.IMS_PER_BATCH = 24
+    cfg.SOLVER.BASE_LR = (
+        0.02 * cfg.SOLVER.IMS_PER_BATCH / 16
+    )  # The LR is scaled with no. images
 
-    cfg.SOLVER.AMP.ENABLED = False
-    cfg.SOLVER.IMS_PER_BATCH = 4
-    cfg.SOLVER.BASE_LR = 0.02 * cfg.SOLVER.IMS_PER_BATCH / 16
-    EPOCHS = 10
-    cfg.SOLVER.MAX_ITER = len(train_dataset) // cfg.SOLVER.IMS_PER_BATCH * EPOCHS
-    cfg.SOLVER.CHECKPOINT_PERIOD = len(train_dataset) // cfg.SOLVER.IMS_PER_BATCH // 2
+    cfg.TEST.EVAL_PERIOD = len_epoch // cfg.SOLVER.IMS_PER_BATCH
+    cfg.SOLVER.MAX_ITER = len_epoch // cfg.SOLVER.IMS_PER_BATCH * epochs
+    cfg.SOLVER.CHECKPOINT_PERIOD = len_epoch // cfg.SOLVER.IMS_PER_BATCH // 2
     cfg.SOLVER.STEPS = (
         int(cfg.SOLVER.MAX_ITER * 0.6),
         int(cfg.SOLVER.MAX_ITER * 0.8),
     )
-    cfg.TEST.EVAL_PERIOD = len(train_dataset) // cfg.SOLVER.IMS_PER_BATCH
 
-    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 10
     cfg.OUTPUT_DIR = "./output"
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 10
+
+    return cfg
+
+
+if __name__ == "__main__":
+    setup_logger()
+
+    train_dataset = make_coco_dicts("train")
+    val_dataset = make_coco_dicts("val")
+
+    EPOCHS = 10
+    cfg = get_custom_config(EPOCHS, len(train_dataset))
+
+    # TODO Remove magic strings
+    cfg.DATASETS.TRAIN = ("bdd_dataset_train",)
+    cfg.DATASETS.TEST = ("bdd_dataset_val",)
+
     # cfg.MODEL.WEIGHTS = "./output/model_0004999.pth"
 
     trainer = BDDTrainer(cfg)
@@ -62,9 +73,9 @@ if __name__ == "__main__":
     vis_hook = TopKLossVisualizationHook(
         cfg=cfg,
         dataset_name=cfg.DATASETS.TEST[0],
-        eval_period=5,
-        topk=5,
-        num_fixed_samples=5,
+        eval_period=len(train_dataset) // cfg.SOLVER.IMS_PER_BATCH // 2,
+        topk=10,
+        num_fixed_samples=10,
     )
     trainer.register_hooks([vis_hook])
     trainer.resume_or_load(resume=False)
